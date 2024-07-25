50388790d8cb005e489aa8c70470ab050f7e0168
t.schroeer@double-d-it.de <t.schroeer@double-d-it.de>
2024-07-25 10:55:52 +0200

    Components Kontakt und Impressum fertig

--- src/app/components/impressum/impressum.component.ts
+++ src/app/components/impressum/impressum.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './impressum.component.scss'
 })
 export class ImpressumComponent {
-
+  emailWebmaster = 'webmaster@kegelgruppe-kepa.de';
 }
--- src/app/components/kontakt/kontakt.component.ts
+++ src/app/components/kontakt/kontakt.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {
@@ -8,16 +8,17 @@ import {
   Validators,
   FormsModule,
   ReactiveFormsModule,
+  FormGroup
 } from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 
 /** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+// export class MyErrorStateMatcher implements ErrorStateMatcher {
+//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+//   }
+// }
 
 @Component({
   selector: 'app-kontakt',
@@ -27,9 +28,28 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrl: './kontakt.component.scss'
 })
 export class KontaktComponent {
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  nameFormControl = new FormControl('', [Validators.required]);
-  nachrichtFormControl = new FormControl('', [Validators.required]);
+  // emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  // nameFormControl = new FormControl('', [Validators.required]);
+  // nachrichtFormControl = new FormControl('', [Validators.required]);
 
-  matcher = new MyErrorStateMatcher();
+  // matcher = new MyErrorStateMatcher();
+
+  @ViewChild(NgForm) private ngForm!: NgForm;
+  private form!: FormGroup;
+
+  sendEmail(value: any){
+    console.log(value);
+  }
+
+  getMailBody(value: any) : string{
+    //const txtBody = 'Name = ' + value.Name + "\n\rTelefon = " + value.Telefon + "\n\rEmail = " + value.Email + '\n\rNachricht:\n\r\n\r' + value.Nachricht;
+    const txtBody = `Name = ${value.Name}
+                     Telefon = ${value.Telefon} 
+                     Email = ${value.Email} 
+                     Nachricht:
+                     ${value.Nachricht}`;
+    console.log("txtBody = " + txtBody);
+    
+    return txtBody;
+  }
 }
